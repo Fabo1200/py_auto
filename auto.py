7e95d26c963726e43d354ab023ec21eafc75c836
Fabian Staegemann <25303665+Fabo1200@users.noreply.github.com>
2023-02-26 12:23:40 +0100

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,23 @@
+import pyautogui
+import keyboard
+
+print("Press and hold the p button!")
+times_pressed = 0
+messages = 0
+
+while True:
+    if keyboard.is_pressed('p'):
+        # Get the current position of the mouse
+        x, y = pyautogui.position()
+
+        # Press the left mouse button at the current position
+        pyautogui.mouseDown(x, y)
+
+        # Release the left mouse button at the same position
+        pyautogui.mouseUp(x, y)
+        times_pressed += 1
+
+        # Print a message every 100 left mouse button presses
+        if times_pressed % 100 == 0:
+            messages += 1
+            print("Pressed Left Mouse Button: ", times_pressed, " times!")
